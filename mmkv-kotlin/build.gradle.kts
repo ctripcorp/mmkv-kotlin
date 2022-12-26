65d6e0fe3f23d5620b981310b794198e1239e47f
qiaoyuang <qiaoyuang2012@gmail.com>
2022-12-26 19:48:07 +0800

    Signing use the memory mode

--- mmkv-kotlin/build.gradle.kts
+++ mmkv-kotlin/build.gradle.kts
@@ -171,6 +171,10 @@ publishing {
         }
     }
     signing {
+        val signingKeyId: String by project
+        val signingKey: String by project
+        val signingPassword: String by project
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
         sign(publishing.publications)
     }
 }
