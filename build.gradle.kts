0c686b434c735b50c005ec9f657810898f1551c4
qiaoyuang <qiaoyuang2012@gmail.com>
2025-07-06 21:38:51 +0100

    Fix MavenCentral publishing process

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.multiplatform) apply false
     alias(libs.plugins.cocoapods) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.vanniktech.maven.publish) apply false
 }
