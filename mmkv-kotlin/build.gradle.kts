dae963b26c6fd03401c0f74e4b16162e02f1afc4
qiaoyuang <qiaoyuang2012@gmail.com>
2025-07-06 21:38:51 +0100

    Fix MavenCentral publishing process

--- mmkv-kotlin/build.gradle.kts
+++ mmkv-kotlin/build.gradle.kts
@@ -6,8 +6,7 @@ plugins {
     alias(libs.plugins.cocoapods)
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.parcelize)
-    alias(libs.plugins.maven.publish)
-    signing
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 version = "1.2.18"
@@ -15,6 +14,7 @@ group = "com.ctrip.flight.mmkv"
 
 @OptIn(ExperimentalKotlinGradlePluginApi::class)
 kotlin {
+    jvmToolchain(21)
     androidTarget {
         publishLibraryVariants("release")
         instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
@@ -72,63 +72,45 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_21
+        isCoreLibraryDesugaringEnabled = true
     }
 }
 
-val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
+dependencies {
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
 
-publishing {
-    publications.withType<MavenPublication> {
-        artifact(javadocJar)
-        with(pom) {
-            name.set("MMKV-Kotlin")
-            description.set("MMKV for Kotlin Multiplatform")
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "mmkv-kotlin",
+        version = version.toString(),
+    )
+
+    pom {
+        name.set("MMKV-Kotlin")
+        description.set("MMKV for Kotlin Multiplatform")
+        url.set("https://github.com/ctripcorp/mmkv-kotlin")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("qiaoyuang")
+                name.set("Yuang Qiao")
+                email.set("qiaoyuang2012@gmail.com")
+            }
+        }
+        scm {
             url.set("https://github.com/ctripcorp/mmkv-kotlin")
-            licenses {
-                license {
-                    name.set("The Apache License, Version 2.0")
-                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                }
-            }
-            developers {
-                developer {
-                    id.set("qiaoyuang")
-                    name.set("Yuang Qiao")
-                    email.set("qiaoyuang2012@gmail.com")
-                }
-            }
-            scm {
-                url.set("https://github.com/ctripcorp/mmkv-kotlin")
-                connection.set("scm:git:https://github.com/ctripcorp/mmkv-kotlin.git")
-                developerConnection.set("scm:git:https://github.com/ctripcorp/mmkv-kotlin.git")
-            }
+            connection.set("scm:git:https://github.com/ctripcorp/mmkv-kotlin.git")
+            developerConnection.set("scm:git:https://github.com/ctripcorp/mmkv-kotlin.git")
         }
     }
-    repositories {
-        maven {
-            credentials {
-                val NEXUS_USERNAME: String by project
-                val NEXUS_PASSWORD: String by project
-                username = NEXUS_USERNAME
-                password = NEXUS_PASSWORD
-            }
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-        }
-    }
-    signing {
-        val SIGNING_KEY_ID: String by project
-        val SIGNING_KEY: String by project
-        val SIGNING_PASSWORD: String by project
-        useInMemoryPgpKeys(SIGNING_KEY_ID, SIGNING_KEY, SIGNING_PASSWORD)
-        sign(publishing.publications)
-    }
-}
-
-// TODO: remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
-project.tasks.withType(AbstractPublishToMaven::class.java).configureEach {
-    dependsOn(project.tasks.withType(Sign::class.java))
 }
